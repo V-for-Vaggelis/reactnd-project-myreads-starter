3388fbf2962e162ee4d3b8d5ed25376028c6bb29
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-06-26 01:41:42 +0300

    Added status property to books and make state only contain one array

--- src/ShelfDisplay.js
+++ src/ShelfDisplay.js
@@ -4,7 +4,7 @@ import BookDisplay from "./BookDisplay"
 function ShelfDisplay (props) {
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Read</h2>
+      <h2 className="bookshelf-title">{props.shelfName}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {props.books.map((book) => (
