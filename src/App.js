f57df10bf6cd620bf83270b7c9ae54c7363c5d23
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-06-27 16:41:57 +0300

    Replaces short circuit code for page tracking with Route components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,11 @@ import './App.css'
 import SearchBook from './SearchBook'
 import ShelfDisplay from './ShelfDisplay'
 import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-    /**
-    * TODO: Instead of using this state variable to keep track of which page
-    * we're on, use the URL in the browser's address bar. This will ensure that
-    * users can use the browser's back and forward buttons to navigate between
-    * pages, as well as provide a good URL they can bookmark and share.
-    */
-    showSearchPage: false
+    books: []
   }
   // This will ensure the shelved books are pulled from the server
   componentDidMount() {
@@ -35,30 +29,28 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBook onBack={this.showHomePage}/>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <ShelfDisplay bookRemove={this.removeBook} shelfName="Currently reading" books={this.state.books.filter((book) => book.shelf === "currentlyReading")} />
-                <ShelfDisplay bookRemove={this.removeBook} shelfName="Want to read" books={this.state.books.filter((book) => book.shelf === "wantToRead")} />
-                <ShelfDisplay bookRemove={this.removeBook} shelfName="Read" books={this.state.books.filter((book) => book.shelf === "read")} />
+        <Route exact path='/' render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <ShelfDisplay bookRemove={this.removeBook} shelfName="Currently reading" books={this.state.books.filter((book) => book.shelf === "currentlyReading")} />
+                  <ShelfDisplay bookRemove={this.removeBook} shelfName="Want to read" books={this.state.books.filter((book) => book.shelf === "wantToRead")} />
+                  <ShelfDisplay bookRemove={this.removeBook} shelfName="Read" books={this.state.books.filter((book) => book.shelf === "read")} />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to='/search'>Add a book</Link>
               </div>
             </div>
-            <div className="open-search">
-              {/* <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a> */}
-              <Link to='/search'>Add a book</Link>
-            </div>
-          </div>
-        )}
-      </div>
-    )
+          )}/>
+          <Route path='/search' component={SearchBook}/>
+        </div>
+      )
+    }
   }
-}
 
 
-export default BooksApp
+  export default BooksApp
