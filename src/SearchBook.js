d5bb833d208fc36ae5ad40e185ef81852ca9ebd8
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-06-28 19:42:47 +0300

    Made input communicate input-value to search page state

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 class SearchBook extends Component {
+  state = {
+    query: ""
+  }
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
   render() {
     return (
       <div className="search-books">
@@ -16,8 +22,13 @@ class SearchBook extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
               */}
-              <input type="text" placeholder="Search by title or author"/>
-
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(e) => this.updateQuery(e.target.value)}
+                />
+              {JSON.stringify(this.state)}
             </div>
           </div>
           <div className="search-books-results">
