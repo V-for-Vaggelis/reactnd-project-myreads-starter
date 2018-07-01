32e8788a2989ef41ed8503e2f348fdb69bab5ff4
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-02 02:05:47 +0300

    Added checkbox on book's active shelf

--- src/BookDisplay.js
+++ src/BookDisplay.js
@@ -8,26 +8,36 @@ function BookDisplay (props) {
     height: 192,
     backgroundImage: `url(${props.thumb})`
   };
-return (
-  <li>
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={divStyle}></div>
-        <div className="book-shelf-changer">
-          <select defaultValue={props.bookToShow.shelf} onChange={(e) => props.onChangeShelf(props.bookToShow, e.target.value)}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+  // This function will add a check to the book's active shelf
+  let checkValue = (val) => {
+    if (val === props.bookToShow.shelf) {
+      // In React we can't insert unicodes with their html code so we use C/C++/Java code
+      return "\u2705 "
+    }
+    else {
+      return
+    }
+  }
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={divStyle}></div>
+          <div className="book-shelf-changer">
+            <select defaultValue={props.bookToShow.shelf} onChange={(e) => props.onChangeShelf(props.bookToShow, e.target.value)}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading {checkValue("currentlyReading")}</option>
+              <option value="wantToRead">Want to Read {checkValue("wantToRead")}</option>
+              <option value="read">Read {checkValue("read")}</option>
+              <option value="none">None {checkValue("none")}</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{props.bookToShow.title}</div>
+        <div className="book-authors">{props.bookToShow.authors}</div>
       </div>
-      <div className="book-title">{props.bookToShow.title}</div>
-      <div className="book-authors">{props.bookToShow.authors}</div>
-    </div>
-  </li>
-)
+    </li>
+  )
 }
 
 export default BookDisplay
